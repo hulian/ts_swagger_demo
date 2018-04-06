b46a5e694e996c403a3eb64b370488ef0fcf4189
hulian <hu.lian.cn@gmail.com>
2018-04-06 12:41:35 +0800

    ggg

--- js/app.js
+++ js/app.js
@@ -15,8 +15,5 @@ SwaggerExpress.create(config, function (err, swaggerExpress) {
     swaggerExpress.register(app);
     var port = process.env.PORT || 10010;
     app.listen(port);
-    if (swaggerExpress.runner.swagger.paths['/hello']) {
-        console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-    }
 });
 //# sourceMappingURL=app.js.map
