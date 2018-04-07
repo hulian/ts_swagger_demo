4c458a4eaba793415e9fdbf561f5793b4f911ea6
hulian <hu.lian.cn@gmail.com>
2018-04-07 19:17:24 +0800

    tt

--- ts/test/TestController.ts
+++
@@ -1,9 +0,0 @@
-import * as log4j from "log4js";
-
-class TestController{
-    public seyHello( name:String){
-        log4j.getLogger().info(name+"test");
-    }
-} 
-var instance = new TestController();
-export = instance;   
